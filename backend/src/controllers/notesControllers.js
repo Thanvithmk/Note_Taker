e8eaa2ffe914f1a5538a1c0f09644304cb059d93
Thanvithmk <thanvithmk@gmail.com>
2025-08-31 11:32:24 +0530

    delete feature added api

--- backend/src/controllers/notesControllers.js
+++ backend/src/controllers/notesControllers.js
@@ -2,13 +2,25 @@ import Note from "../models/Note.js";
 
 export async function getAllNotes(req, res) {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({createdAt:-1});
     res.status(200).json(notes);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Something went wrong" });
   }
-}
+};
+
+export async function getNoteByID(req, res) {
+  try {
+    const note = await Note.findById(req.params.id);
+    if(!note) return res.status(404).json({message:"Note not found"})
+    res.status(200).json(note);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
 
 export async function createNote(req, res) {
   try{
@@ -21,12 +33,29 @@ export async function createNote(req, res) {
     console.log(err);
     res.status(500).json({message:"Something went wrong..."});
   }
-}
+};
 
-export function updateNote(req, res) {
-  res.status(200).json({ message: "Note updated successfully" });
-}
+export async function updateNote(req, res) {
+  try{
+    const {title,content}=req.body;
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id,{title,content},{new:true});
 
-export function deleteNote(req, res) {
-  res.status(200).json({ message: "Note deleted successfully" });
+    if(!updatedNote) return res.status(404).json({message:"Note not found"})
+      
+    res.status(200).json(updatedNote);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message:"Something went wrong..."});
+  }
+};
+
+export async function deleteNote(req, res) {
+  try{
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedNote);
+  }
+ catch(err){
+  console.log(err)
+  res.status(500).json({message:"Something went wrong..."});
+ }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -13,8 +13,6 @@ app.use("/api/notes", notesRoutes);
 
 connectDB();
 
-
-
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
