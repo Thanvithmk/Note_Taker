5914fdd70e9b97d1fbdbe68b01b8ea6d261e0c78
Thanvithmk <thanvithmk@gmail.com>
2025-08-31 12:03:47 +0530

    hhh

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
-app.use(express.json());  //middleware
-app.use("/api/notes", notesRoutes);
-
 connectDB();
+
+app.use(express.json());  //middleware : this one is to convert the data into required format before sending it to controller
+
+app.use((req,res,next)=>{
+  console.log("hello");
+  next();
+})
+
+app.use("/api/notes", notesRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
