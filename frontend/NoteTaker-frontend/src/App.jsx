cab93d2ad68c0210a45f644ba95f0fcca6f85370
Thanvithmk <thanvithmk@gmail.com>
2025-08-31 13:17:15 +0530

    added toaster text button

--- frontend/NoteTaker-frontend/src/App.jsx
+++ frontend/NoteTaker-frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetail from './pages/NoteDetail'
+import { Routes, Route } from "react-router-dom"
+import toast from 'react-hot-toast'
 const App = () => {
   return (
     <div>
-      
+      <button onClick={() => toast.success("success")}>click</button>
+      <button onClick={() => toast.error("error")}>click</button>
+      <Routes>
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/create" element={<CreatePage/>}/>
+        <Route path="/note/:id" element={<NoteDetail/>}/>
+      </Routes>
     </div>
   )
 }
