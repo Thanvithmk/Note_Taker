abd33facc338a43108ce8a4a4c14b950eb175b24
Thanvithmk <thanvithmk@gmail.com>
2025-08-31 12:37:04 +0530

    rate limiter added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,25 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "../config/db.js";
+import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-connectDB();
+
 
 app.use(express.json());  //middleware : this one is to convert the data into required format before sending it to controller
-
-app.use((req,res,next)=>{
-  console.log("hello");
-  next();
-})
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
+//connect to database and then start the server
+//they can be aslo written seperately
+connectDB().then(()=>{
+  app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
+})
+
