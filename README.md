a1e0538069a8544d94cde99a446b5309176ba1eb
Thanvithmk <thanvithmk@gmail.com>
2025-09-02 18:23:34 +0530

    Deployment setup in index.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#A full-stack Note Taking Application built with the MERN stack (MongoDB, Express, React, Node.js).
+A full-stack Note Taking Application built with the MERN stack (MongoDB, Express, React, Node.js).
 Users can create, view, update, and delete notes. The app is designed with a simple and 
 responsive UI and deployed for production use.
 
@@ -11,11 +11,11 @@ responsive UI and deployed for production use.
 🌐 Fully deployed (Frontend + Backend + Database)
 
 🛠️ Tech Stack
-Frontend: React, React Router, Tailwind/DaisyUI (if used)
+Frontend: React, React Router, Tailwind/DaisyUI
 Backend: Node.js, Express.js
-Database: MongoDB (Atlas or local)
+Database: MongoDB (Atlas)
 
 Deployment:
-Frontend → Vercel / Netlify
-Backend → Render / Railway / Heroku
+Frontend → render
+Backend → Render 
 Database → MongoDB Atlas
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,18 +11,31 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname=path.resolve();   //frontend path
+
 
 // Middleware FIRST
-app.use(cors({
+if(process.env.NODE_ENV!='production'){
+  app.use(cors({
   origin: "http://localhost:5173", // React dev server
   credentials: true
 }));
+}
 
 app.use(express.json()); // for parsing JSON bodies
 app.use(rateLimiter);    // apply before routes
 
 // Routes AFTER middleware
 app.use("/api/notes", notesRoutes);
+
+
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+  });
+}
 
 // DB connect + start server
 connectDB().then(() => {
