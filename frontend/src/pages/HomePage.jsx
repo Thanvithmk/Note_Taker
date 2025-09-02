f45a09f76fb37b93e41794aa27090f1b9a0343aa
Thanvithmk <thanvithmk@gmail.com>
2025-09-02 18:46:11 +0530

    footer

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import api from "../lib/axios";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
 import NotesNotFound from "../components/NotesNotFound";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 
 
 const HomePage = () => {
